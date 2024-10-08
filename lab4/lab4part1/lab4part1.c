9ee96f9b0c06f80adf2d872ecae9b0f4772490df
180Sai <92890058+180Sai@users.noreply.github.com>
2024-10-08 13:23:19 -0400

    lab 4 part 1, unfinished part 4

--- lab4/lab4part1/lab4part1.c
+++ lab4/lab4part1/lab4part1.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+/**
+ * 2024-10-08 ENGG-1410 Lab 4
+ * Implements the series that estimates pi.
+ * Calculates the value based on the number of terms.
+ */
 int main(void)
 {
-    printf("This is a test.");
+    int termCount;
+    double pi = 0;
+    printf("Enter the number of terms for pi approximation: ");
+    scanf("%d", &termCount);
+
+    for (int i = 0; i <= termCount; i++)
+    {
+        pi += (i % 2 == 0 ? 1 : -1) / ((double)1 + i * 2);
+    }
+
+    pi *= 4;
+    printf("Estimated value of pi after %d terms: %.3f", termCount, pi);
 
     return 0;
 }
