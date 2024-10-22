06f1845e707fbae87d56e9ef4af84385f95d4413
180Sai <92890058+180Sai@users.noreply.github.com>
2024-10-22 13:55:03 -0400

    complete base conversion header

--- lab5/base.h
+++ lab5/base.h
@@ -1,1 +1,31 @@
 #pragma once
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * Converts a number from a base-b to base-10.
+ */
+int convertToFromBase10(int n, int b, int istoTen)
+{
+    int b2;
+    if (istoTen)
+    {
+        b2 = b;
+        b = 10;
+    }
+    else
+    {
+        b2 = 10;
+    }
+
+    int nResult = 0;
+    for (int power = 0; n > 0; power++)
+    {
+        int digit = n % b;
+        n /= b;
+        nResult += digit * (int)pow(b2, power);
+        ;
+    }
+
+    return nResult;
+}
