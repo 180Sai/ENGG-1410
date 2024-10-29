998973a346eb0bbdfcf38a4c65f0b92946673167
180Sai <92890058+180Sai@users.noreply.github.com>
2024-10-28 20:19:40 -0400

    fix typo

--- lab5/arithmetic.h
+++ lab5/arithmetic.h
@@ -1,20 +1,17 @@
 #pragma once
 #include "base.h"
 
-int ctofb10(int n, int b, int istoTen) {
-  return convertToOrFromBase10(n, b, istoTen);
-}
+int operate(char o, int b1, int n1, int n2, int b2)
+{
+  n1 = ctofb10(n1, b1, 1); // take the first number and convert it from base b1 to base 10
+  n2 = ctofb10(n2, b1, 1); // take the second number and convert it from to base 10
 
-int operate(char o, int b1, int n1, int n2, int b2) {
-  n1 = convertToOrFromBase10(n1, b1, 1);  // take the first number and convert it to base 10
-  n2 = convertToOrFromBase10(n2, b1, 1);  // take the second number and convert it to base 10
-
-  // nested ternary operator that applys the correct operation and changes base
-  // into output base
-  return o == '+' ? ctofb10(add(n1, n2), b2, 0)
-                  : (o == '-' ? ctofb10(sub(n1, n2), b2, 0)
-                              : (o == '*' ? ctofb10(mult(n1, n2), b2, 0)
-                                          : ctofb10(div(n1, n2), b2, 0)));
+  // nested ternary operator switches between different operators
+  // returns final number to desired base (b2)
+  return o == '+' ? ctofb10(add(n1, n2), b2, 0)                            // operator is '+': convert sum of n1, n2 from base 10 to base b2
+                  : (o == '-' ? ctofb10(sub(n1, n2), b2, 0)                // operator is '-': convert difference of n1-n2 to base b2
+                              : (o == '*' ? ctofb10(mult(n1, n2), b2, 0)   // operator is '*': convert product of n1, n2 to base b2
+                                          : ctofb10(div(n1, n2), b2, 0))); // operator is (implied) '/': convert quotient of n1/n2 to base b2
 }
 
 // add a and b
--- lab5/base.h
+++ lab5/base.h
@@ -4,21 +4,32 @@
 
 /**
  * Converts a number from a base-b to base-10.
+ *
+ * Numbers can be converted from base-10 to base-b by repeatedly:
+ * (a) dividing the base-10 number by b and
+ * (b) multiplying the remainder by increasing powers of 10, starting from 0.
+ *
+ * Converting from base-b to base-10 instead divides by 10 and multiplies the remainder by increasing powers (start from 0) of b.
  */
-int convertToOrFromBase10(int n, int b, int istoTen) {
+int ctofb10(int n, int b, int istoTen)
+{
   int b2;
-  if (istoTen) {
-    b2 = b;
-    b = 10;
-  } else {
+  if (istoTen) // if true then convert base-b to 10, else convert to base-10 to b
+  {
+    b2 = b; // b2 is the base to start from
+    b = 10; // b is the base to end with (divisor)
+  }
+  else
+  {
     b2 = 10;
   }
 
-  int nResult = 0;
-  for (int power = 0; n > 0; power++) {
+  int nResult = 0;                    // final number
+  for (int power = 0; n > 0; power++) // keeep dividing by b until n is 0; increment power by 1 every iteration
+  {
     int digit = n % b;
     n /= b;
-    nResult += digit * (int)pow(b2, power);
+    nResult += digit * (int)pow(b2, power); // multiply remainder by the b2^power
   }
 
   return nResult;
