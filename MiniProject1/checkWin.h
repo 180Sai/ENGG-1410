9dd409dd1be50dc4561435794d939a5990928918
jere-dev <jeremthom1@gmail.com>
2024-11-03 01:53:39 -0400

    fixed checkWin function

--- MiniProject1/checkWin.h
+++ MiniProject1/checkWin.h
@@ -3,49 +3,51 @@
 
 int checkWin(char board[MAX_SIZE][MAX_SIZE], int size)
 {
-    for (int i = 0; i < size; i++)
+    for (int row = 0; row < size; row++)
     {
-        for (int j = 1; j < size; j++)
+        for (int col = 1; col < size; col++)
         {
-            if (board[i][j] != ' ')
-                if (board[i][j] == board[i][0])
-                    break;
-            if (j == size - 1)
+            if (board[row][col] == ' ')                 //if space empty don't check row no more
+                break;
+            if (board[row][col] != board[row][0])         //if space not equal to corrner piece dont check row no more
+                break;
+            if (col == size - 1)                      //if j is at end, this means that row is vaild
                 return 1;
         }
     }
 
-    for (int i = 0; i < size; i++)
+    for (int col = 0; col < size; col++)
     {
-        for (int j = 1; j < size; j++)
+        for (int row = 1; row < size; row++)
         {
-            if (board[j][i] != ' ')
-                if (board[j][i] == board[0][i])
-                    break;
-
-            if (j == size - 1)
+            if (board[row][col] == ' ')                 //if space empty don't check col no more
+                break;
+            if (board[row][col] != board[0][col])         //if space not equal to corrner piece dont check col no more
+                break;
+            if (row == size - 1)                      //if j is at end, this means that col is vaild
                 return 1;
         }
     }
 
-    for (int i = 0; i < size; i++)
+    for(int i = 0; i < size; i++)
     {
-        if (board[i][i] == ' ')
+        if (board[i][i] == ' ')                 //if space empty don't check col no more
             break;
-        if (board[i][i] != board[i][0])
+        if (board[i][i] != board[0][0])         //if space not equal to corrner piece dont check col no more
             break;
-        if (i == size - 1)
+        if(i == size-1)
             return 1;
     }
 
-    for (int i = size - 1; i >= 0; i--)
+    for(int i = 0; i < size; i++)
     {
-        if (board[i][i] == ' ')
+        if (board[i][size-i-1] == ' ')                 //if space empty don't check col no more
             break;
-        if (board[i][i] != board[i][size - 1])
+        if (board[i][size-i-1] != board[0][size-1])         //if space not equal to corrner piece dont check col no more
             break;
-        if (i == 0)
+        if(i == size-1)
             return 1;
     }
+
     return 0;
 }
