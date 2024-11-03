e5cc80172747fa1057ff835ab1b479533a8bdb7d
jere-dev <jeremthom1@gmail.com>
2024-11-03 01:51:08 -0400

    simplified initializeBoard function

--- MiniProject1/init.h
+++ MiniProject1/init.h
@@ -1,13 +1,6 @@
 #pragma once
 #include "globalVar.h"
 
-void initializeBoard(char board[MAX_SIZE][MAX_SIZE], int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            board[i][j] = ' ';
-        }
-    }
+void initializeBoard(char board[MAX_SIZE][MAX_SIZE], int size) {
+    for (int i = 0; i < size; i++) for (int j = 0; j < size; j++) board[i][j] = ' '; //set all zones on board to be a space
 }
