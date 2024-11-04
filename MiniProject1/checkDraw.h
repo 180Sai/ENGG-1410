84814ef8835347896b68e13e67021126d9edf642
jere-dev <jeremthom1@gmail.com>
2024-11-04 02:25:34 -0500

    made checkDraw function

--- MiniProject1/checkDraw.h
+++ MiniProject1/checkDraw.h
@@ -1,4 +1,8 @@
 #pragma once
 #include "globalVar.h"
 
-int checkDraw(char board[MAX_SIZE][MAX_SIZE], int size);
+int checkDraw(char board[MAX_SIZE][MAX_SIZE], int size)
+{
+    for(int row = 0; row < size; row++) for (int col = 0; col < size; col++) if(board[row][col] == ' ') return 0;
+    return 1;
+}
