4306f0b41e9da1047327a7bb7b24b44060e893b6
jere-dev <jeremthom1@gmail.com>
2024-11-14 16:07:05 -0500

    file read, encrypt error unresolved

--- MiniProject2/main.c
+++ MiniProject2/main.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
+#include <string.h>
 
+#include "decihex.h"
 
-int main(int argc, char** argv[])
+int main(int argc, const char* argv[])
 {
-    printf(argv[1]);
+    int file_index = 1;
+    int isEncrypt = 1;
+
+    if(argc > 2){
+        if(strcmp(argv[1], "-E") != 0){
+            isEncrypt = 0;
+            }
+        file_index = 2;
+    }
+
+    FILE* file_handle;
+    file_handle = fopen(argv[file_index], "r");
+
+    char fileText[100];
+
+    while (fgets(fileText, sizeof(fileText), file_handle) != NULL) {
+        if (isEncrypt){
+            for(int i = 0; i < sizeof(fileText); i++)
+            {
+                if (fileText[i] == '\n')
+                    printf("\n------------");
+                else if (fileText[i] == '\t')
+                    printf("TT");
+                else if (fileText[i]-16 < 32)
+                {
+                    char h[digiDecihex(fileText[i]-16-32+144)];
+                    h[0] = fileText[i]-16-32+144;
+                    h[1] = '\0';
+                    decihex(fileText[i]-16-32+144, h);
+                    printf("%s, %c, %d\n", h, fileText[i]-16-32+144, (fileText[i]-16-32+144));
+                }
+                else{
+                    char h[digiDecihex(fileText[i]-16)];
+                    h[0] = fileText[i]-16;
+                    h[1] = '\0';
+                    decihex(digiDecihex(fileText[i]-16), h);
+                    printf("%s, %c, %didk\n", h, fileText[i]-16, (fileText[i]-16-32+144));
+                }
+            }
+        }
+        else{
+
+        }
+    }
+
+    fclose(file_handle);
+    printf("%d, %c", 'A'-16, 'A'-16);
 }
